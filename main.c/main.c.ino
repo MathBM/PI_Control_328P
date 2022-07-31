2edd3fa3ed9a7c2265450acbfc93c4af4b881b3a
Munzi <matheusbarbierimunzi@hotmail.com>
2022-07-31 17:03:36 -0300

    ":construction: Timer and Conversor AD"

--- main.c/main.c.ino
+++ main.c/main.c.ino
@@ -1,3 +1,10 @@
+//! Archive Description.
+/*!
+* \author Matheus Barbi. M.
+  \author Rodri Jost. 
+* \since 31/07/2022
+* \version 1.0
+*/
 
 #define tst_bit(Y,bit_x)  (Y&(1<<bit_x)) 
 #define set_bit(Y, bit_x) (Y|=(1<<bit_x))
@@ -6,6 +13,15 @@
 
 unsigned int value = 0;
 
+ISR(TIMER2_OVF_vect)
+{
+  set_bit(ADCSRA, ADSC);
+  while(!(clr_bit(ADCSRA, ADIF)));
+  set_bit(ADCSRA, ADIF);
+
+  value = (ADCH << 8 | ADCL);
+}
+
 void setup() 
 {
 
@@ -13,18 +29,27 @@ void setup()
 
   Serial.begin(9600);
   
+  /*!
+    ADC Config, 128.
+  */
   ADCSRA = 0b10010111;
   ADMUX = 0b01000000;
+
+  /*!
+    T2 OVF MODE, .
+  */
+  TCCR2A = 0x00;
+  TCCR2B = 0x00;
+  TCCR2B |= (1<<CS21)|(1 << CS20); // Configurar PreScaler
+
+  TCNT0 = 0x00;
+
+  set_bit(TIMSK2, TOIE2);
 
   sei();
 }
 
 void loop() 
 {
-  set_bit(ADCSRA, ADSC);
-  while(!(clr_bit(ADCSRA, ADIF)));
-  set_bit(ADCSRA, ADIF);
-
-  value = (ADCH << 8 | ADCL);
   Serial.println(value);
 }
