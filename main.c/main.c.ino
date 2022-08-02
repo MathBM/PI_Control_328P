1a23e0f842553b501c706a61b917897aa68ebdd0
Matheus Munzi <matheusbarbierimunzi@hotmail.com>
2022-08-01 21:56:32 -0300

    :bulb: docs: Comentários sobre a  estrutura do F.W

--- main.c/main.c.ino
+++ main.c/main.c.ino
@@ -1,114 +1,177 @@
-//! Archive Description.
-/*!
+/*
+ * ControlePI-ATMega328p.c
+ *
+ * ! Archive Description.
+*
 * \author Matheus Barbi. M.
   \author Rodri Jost. 
 * \since 31/07/2022
-* \version 1.0
+* \version 1.5
 */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+///! Macros and Control Variables
 
 #define tst_bit(Y,bit_x)  (Y&(1<<bit_x)) 
 #define set_bit(Y, bit_x) (Y|=(1<<bit_x))
 #define clr_bit(Y, bit_x) (Y&=~(1<<bit_x))
 #define cpl_bit(Y, bit_x) (Y^=(1<<bit_x))
 
-#define acionaMotor 1
-#define KP 1.5 
-#define KI 50.00
-#define KA 0.01 //coeficiente de anti wind up ajustado para a montagem
-#define TS 128 //us
+///!  Frequência Interna 8MHZ
+#define F_CPU 8000000UL
 
-unsigned int leituraAnalogica = 0;
-int r[] = {350, 500}; //Operar na região linear
-float u = 0, us[2], e[2], yf[2], usat = 0, k1 = 0, k2 = 0, aw = 0; //usat será, de forma importante, 0 ou 255
-int i = 0, j = 0, k = 0, y[2];
+///! Constante de Controle
+/*!
+    KA  coeficiente de anti wind-up
+    TS  Periodo de amostragem (ms)
+*/
+#define acionaMotor 1 
+#define KP 20 
+#define KI 30.00
+#define KA 0.01 
+#define TS 1 
 
-ISR(TIMER2_OVF_vect)
+///! Variaveis de Controle
+/*!
+    r[]  SET POINT
+*/
+volatile unsigned int leituraAnalogica = 0;
+volatile int r[] = {300, 425}; 
+volatile float u = 0, us[2], e[2], yf[2], usat = 0, k1 = 0, k2 = 0, aw = 0;
+volatile int i = 0, k = 0, j = 0, y[2];
+
+
+ISR(TIMER2_COMPA_vect)
 {
-  set_bit(ADCSRA, ADSC); 
-  while(tst_bit(ADCSRA, ADSC));
-  leituraAnalogica = ADC;
+  set_bit(ADCSRA, ADSC); //requisita leitura do ADC
+  while(tst_bit(ADCSRA, ADSC)); //aguarda termino da leitura
+  leituraAnalogica = ADC; //captura leitura
 
-  /* Sinal Degrau
-  k++;
-   if(k >= 2000){ //talvez aumentar tempo - original 2000
-    k = 0;
-    j++; // j índice do vetor do sinal degrau
-    if(j >= 2) j = 0;
-  }*/
-
-  // swap de variáveis
+  ///! Swap Var. Control
+  /*!
+    e[] guarda os valores anteriores
+  */
   us[1] = us[0];
-  e[1] = e[0]; // Erro Anterior
+  e[1] = e[0]; 
   y[1] = y[0];
   y[0] = leituraAnalogica;
   yf[1] = yf[0];
   
-  // filtro passa-baixas:
+  ///! filtro passa-baixas
   yf[0] = 0.0500*y[1] + 0.9500*yf[1];
 
-  // controle PI:
+  ///! P.I. Control
+   /*!
+    us[0] PWM Signal;
+  */
   e[0] = r[j] - yf[0];
-  us[0] = us[1] + k1*e[0] + k2*e[1]; //calculo do sinal atual de pwm
+  us[0] = us[1] + k1*e[0] + k2*e[1]; 
 
+  ///! Ajusts P.I
+  /*!
+    antiwindup;
+    aw - Sinal excente;
+    us[0] - KA inicialmente 0 [0.5 ate 2];
+  */
   if (us[0] > 255) usat = 255;
   else if (us[0] < 0) usat = 0;
   else usat = us[0];
-
-  aw = usat - us[0];
-  us[0] = us[0] + KA*aw; 
+  aw = usat - us[0]; 
+  us[0] = us[0] + KA*aw; l
     
-  OCR1A = us[0];
-  if(OCR1A > 255) OCR1A = 255;
-  if(OCR1A < 0) OCR1A  = 0;
-  
+  if(us[0] > 255) OCR1A = 255;
+  if(us[0] < 0) OCR1A  = 0;
+  else OCR1A = us[0]; 
+  k++; 
 }
 
-void setup() 
-{
 
+int main(void)
+{
   cli();
 
   //Serial.begin(115200);
   
+  ///! ADC Config
   /*!
-    ADC Config, 128.
+    Prescaler 128;
+    Vref 5V;
   */
-  ADCSRA = 0b10000111;
-  ADCSRB = 0b00000000;
-  ADMUX = 0b01000000;
+  ADCSRA = 0x00;
+  ADCSRB = 0x00;
+  ADMUX = 0x00;
 
+  set_bit(ADCSRA, ADEN);
+  set_bit(ADCSRA, ADPS0);
+  set_bit(ADCSRA, ADPS1);
+  set_bit(ADCSRA, ADPS2);
+  set_bit(ADMUX, REFS0); 
+  
+  ///! TIMER2 Config
   /*!
-    T2 OVF MODE, .8
+      Prescaler 32;
+      COMPA MODE ;
+      TOP 249;
   */
   TCCR2A = 0x00;
   TCCR2B = 0x00;
-  TCCR2B |= (1<<CS21); // Configurar PreScaler
-
+  TIMSK2 = 0x00;
+  set_bit(TCCR2B,CS21); 
+  set_bit(TCCR2B,CS20);
+  set_bit(TCCR2A, WGM21); 
+  OCR2A = 249; 
   TCNT2 = 0x00;
+  set_bit(TIMSK2, OCIE2A); //Habilita interrupção por comparação
 
-  set_bit(TIMSK2, TOIE2);
+  ///! TIMER1 Config
+  /*!
+      Prescaler 1;
+      Fast PWM Mode;
+      TOP 255;
+      OCR1A - Ciclo ativo 0C1A
+  */
+  TCCR1A = 0x00;
+  TCCR1B = 0x00;
+  set_bit(DDRB, acionaMotor); 
+  set_bit(TCCR1A, COM1A1); 
+  set_bit(TCCR1A, WGM11);
+  set_bit(TCCR1B, WGM12);
+  set_bit(TCCR1B, WGM13);
+  set_bit(TCCR1B, CS10); 
+  ICR1 = 255; 
+  OCR1A = 0;
 
-  set_bit(DDRB, acionaMotor);
-  //fast PWM, TOP = ICR1, OC1A e OC1B habilitados
-  TCCR1A = 0b10100010; //PWM não invertido nos pinos OC1A e OC1B
-  TCCR1B = 0b00011001; //liga TC1, prescaler = 1
-  ICR1 = 255; //valor máximo para contagem
-  OCR1A = 0; //controle do ciclo ativo do PWM 0C1A
-
+  ///! PI Control
+   /*!
+      Inicia Varivaeis de controle
+      Ganhos do controlador
+  */
   for(i=0; i<2; i++) yf[i] = 0;
   for(i=0; i<2; i++) y[i] = 0;
   for(i=0; i<2; i++) e[i] = 0;
   for(i=0; i<2; i++) us[i] = 0;
-    
-  // ganhos do controlador:
-  k1 = KP + KI*TS*0.000001;
+  k1 = KP + KI*TS*1e-3;
   k2 = -KP;
 
-  sei();
+  sei(); 
 
-}
-
-void loop() 
-{
-  
+  ///! Mudança de SETPOINT
+  /*!
+   Sinal Degrau aplicado 
+  */
+    
+  while (1) 
+  {
+    if ((k > 10000) && (j == 0))
+    {
+      j++; 
+    }
+    if ((k > 20000) && (j == 1))
+    {
+      j--; 
+      k = 0;
+    }
+	}
 }
